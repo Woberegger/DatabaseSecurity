50d01a0e5e2f5584ac73d66f3d01fd2f6644fb2d
Wolfgang OBEREGGER <Wolfgang.Oberegger@knapp.com>
2025-05-20 19:20:49 +0200

    reduce policy to minimum, do not used specific context

--- scripts/07/07c_virtual_private_db_policy.sql
+++ scripts/07/07c_virtual_private_db_policy.sql
@@ -1,6 +1,8 @@
--- connect to database as sys user, e.g. docker exec -it -u oracle Oracle23Free sqlplus 'JONES/jones1234@IMS'
+-- connect to database as sys user, e.g. docker exec -it -u oracle Oracle23Free sqlplus 'sys/FhIms9999@IMS as sysdba'
 ALTER session SET container=IMS;
-CONNECT JONES/jones1234@IMS;
+
+/* the following is not necessary, it shall only demonstrate, how a specific context upon login can be set, which could later be evaluated in the policy
+   (using named context "EMPCTX" and writing a logon trigger)
 -- specific policies
 CREATE OR REPLACE PACKAGE context_package AS
   PROCEDURE set_context;
@@ -12,7 +14,7 @@ CREATE OR REPLACE PACKAGE BODY context_package IS
     v_ouser  VARCHAR2(30);
     v_id     NUMBER;
   BEGIN
-    DBMS_SESSION.set_context('JONES','SETUP','TRUE');
+    DBMS_SESSION.set_context('EMPCTX','SETUP','TRUE');
     v_ouser := SYS_CONTEXT('USERENV','SESSION_USER');
     
     BEGIN
@@ -22,13 +24,13 @@ CREATE OR REPLACE PACKAGE BODY context_package IS
       FROM   scott.emp
       WHERE  ename = v_ouser;
       
-      DBMS_SESSION.set_context('JONES','DEPT_ID', v_id);
+      DBMS_SESSION.set_context('EMPCTX','DEPT_ID', v_id);
     EXCEPTION
       WHEN NO_DATA_FOUND THEN
-        DBMS_SESSION.set_context('JONES','DEPT_ID', 0);
+        DBMS_SESSION.set_context('EMPCTX','DEPT_ID', 0);
     END;
     
-    DBMS_SESSION.set_context('JONES','SETUP','FALSE');
+    DBMS_SESSION.set_context('EMPCTX','SETUP','FALSE');
   END set_context;
 END context_package;
 /
@@ -36,7 +38,6 @@ END context_package;
 GRANT EXECUTE ON context_package TO PUBLIC;
 CREATE OR REPLACE PUBLIC SYNONYM context_package FOR context_package;
 
-connect sys/FhIms9999 as sysdba
 --
 CREATE OR REPLACE TRIGGER set_security_context
 AFTER LOGON ON DATABASE
@@ -45,7 +46,9 @@ BEGIN
 END;
 /
 
-CREATE OR REPLACE CONTEXT JONES USING context_package;
+CREATE OR REPLACE CONTEXT EMPCTX USING context_package;
+*/
+connect sys/FhIms9999@IMS as sysdba
 --
 BEGIN
   -- activate the lines, when the policy was already existing before
