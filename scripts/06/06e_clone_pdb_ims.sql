0641007a093097956179566911b801e4f2168636
Wolfgang OBEREGGER <Wolfgang.Oberegger@knapp.com>
2025-05-23 18:53:52 +0200

    write prompts about what is happening

--- scripts/06/06e_clone_pdb_ims.sql
+++ scripts/06/06e_clone_pdb_ims.sql
@@ -1,21 +1,27 @@
 -- connect to database as sys user, e.g. docker exec -it -u oracle Oracle23Free sqlplus '/ as sysdba'
 -- e.g. docker cp ~/DatabaseSecurity/scripts/06/06e_clone_pdb_ims.sql Oracle23Free:/tmp/
 -- time docker exec -it -u oracle Oracle23Free sqlplus / as sysdba @/tmp/06e_clone_pdb_ims.sql
--- important: this only works, when you are connected to the CDB and not to other PDB -- should show CDB
+PROMPT IMPORTANT: this only works, when you are connected to the CDB and not to other PDB
 ALTER SESSION SET CONTAINER=cdb$root;
 SHOW CON_NAME;
+PROMPT creating pluggable DB IMS_COPY from IMS ...
 CREATE Pluggable Database IMS_COPY FROM IMS FILE_NAME_CONVERT=('/opt/oracle/oradata/FREE/IMS/','/opt/oracle/oradata/FREE/IMS_COPY/');
--- open the newly created DB
+
+PROMPT opening the newly created DB ...
 ALTER Pluggable Database IMS_COPY OPEN;
--- change to the container to see, if it works
+
+PROMPT change to the container to see, if it works ...
 ALTER session SET container=IMS_COPY;
+PROMPT check, if users SCOTT and IMS are existing in clone, too ...
 SELECT UserName FROM ALL_USERS WHERE UserName IN ('SCOTT','IMS');
--- should find the new files
+PROMPT should find the new datafiles including tablespaces data+indexes, which were added in IMS
 SELECT FILE_NAME FROM dba_data_files;
---
+PROMPT leave container and go back to CDB ...
 ALTER SESSION SET CONTAINER=cdb$root;
+PROMPT close the pluggable DB ...
 ALTER PLUGGABLE DATABASE IMS_COPY CLOSE;
+PROMPT drop the pluggable DB including datafiles
 DROP PLUGGABLE DATABASE IMS_COPY INCLUDING DATAFILES;
--- we expect, that this directory is empty now, as the datafiles were deleted as well
+PROMPT we expect, that this directory is empty now, as the datafiles were deleted as well ...
 host ls /opt/oracle/oradata/FREE/IMS_COPY/
 quit
