7d04c00e1f38c425ff4a27a7e8d8e82cd1ce62e4
Wolfgang OBEREGGER <Wolfgang.Oberegger@knapp.com>
2025-08-22 12:43:06 +0200

    corrected syntax for students collection

--- scripts/10/10d_test_nosql_inj.php
+++ scripts/10/10d_test_nosql_inj.php
@@ -3,7 +3,7 @@ require 'vendor/autoload.php'; // Composer MongoDB driver
 
 $client = new MongoDB\Client('mongodb://admin:my-secret-pw@localhost:27017/?authSource=admin');
 $db = $client->selectDatabase('ims');
-$collection = $db->selectCollection->students;
+$collection = $db->selectCollection('students');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"]; // pass something like  { "$ne": null }
