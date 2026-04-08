4c2be882a1abb177b0519960563950a85bb1f58f
Wolfgang OBEREGGER <Wolfgang.Oberegger@knapp.com>
2026-04-08 15:03:36 +0200

    audit trigger tested and added prompts to show, what is happening

--- scripts/03/03a_install_audit_trigger.md
+++ scripts/03/03a_install_audit_trigger.md
@@ -11,14 +11,16 @@ cd /tmp
 git clone https://github.com/2ndQuadrant/audit-trigger
 $CONTAINERCMD cp audit-trigger/audit.sql Postgres:/tmp/
 # IMPORTANT: set -d parameter to switch to the correct database, where the audit schema shall be installed
-$CONTAINERCMD exec -it -u postgres Postgres psql -d dvdrental -f /tmp/audit.sql
+$CONTAINERCMD exec -it -u postgres Postgres psql -d ims -f /tmp/audit.sql
 ```
 
 ## use audit trigger
 
 the use do the audit trigger can be shown via the following script, which you can either call in GUI like `pgadmin`<br>
-or in `psql` command line with following syntax:
+or in `psql` command line with following syntax:<br>
+(we better first copy the script into the container, so that we can call it with `-f` parameter and use the \prompt commands to step through the script)
 ```bash
-cat ~student/DatabaseSecurity/scripts/03/03b_use_audit_trigger.sql | $CONTAINERCMD exec -i --tty=false -u postgres Postgres psql -d dvdrental 
+$CONTAINERCMD cp ~student/DatabaseSecurity/scripts/03/03b_use_audit_trigger.sql Postgres:/tmp/
+$CONTAINERCMD exec -it -u postgres Postgres psql -d ims -f /tmp/03b_use_audit_trigger.sql
 ```
 [](https://github.com/Woberegger/DatabaseSecurity/blob/main/scripts/03/03b_use_audit_trigger.sql)
--- scripts/03/03b_use_audit_trigger.sql
+++ scripts/03/03b_use_audit_trigger.sql
@@ -1,24 +1,37 @@
--- monitor changes in the following 3 tables
--- we do this as "postgres" user, who owns the tables
-\c dvdrental
+-- monitor changes in the 3 tables with audit_trigger in IMS database
+\c ims
+\echo monitor changes in tables: address, customer
+\echo we do this as "postgres" user, who owns the tables
 set role postgres;
+\echo we set search path to "dvd, public", so that we do not always need to provide the schema prefix
 SET search_path TO dvd, public;
+\echo enable auditing on address and customer table
+\prompt 'Press <ENTER> to continue ...' dummy
 SELECT audit.audit_table('address');
 SELECT audit.audit_table('customer');
--- the update grants we already gave in previous lecture, now give also insert grants
-GRANT INSERT, DELETE ON address TO objectowner;
--- even for the auto-incremented sequence we need to give rights
+\echo the update grants an "address" we re-do from previous lecture (on certain columns only), now we give also select/insert/delete grants to objectowner
+\prompt 'Press <ENTER> to continue ...' dummy
+GRANT SELECT, INSERT, DELETE ON address TO objectowner;
+GRANT UPDATE (address, postal_code, city_id) ON address TO objectowner;
+\echo even for the auto-increment sequence we need to give rights
 GRANT USAGE ON address_address_id_seq TO objectowner;
--- and now as other user we try to change some data in the tables
+\echo and now as user "objectowner" we try to change some data in the tables
+\prompt 'Press <ENTER> to continue ...' dummy
 set role objectowner;
 SET search_path TO dvd, public;
--- on select nothing happens in trigger tables, but we want to see some data, which we might want to change
+\echo on select from address nothing happens in trigger tables, but we want to see some data, which we might want to change
+\prompt 'Press <ENTER> to continue ...' dummy
 select * from address limit 5;
--- this user has modification rights only on certain columns
+\echo this user has modification rights only on all involved columns, so the following update should work
+\prompt 'Press <ENTER> to continue ...' dummy
 UPDATE Address SET Address='ChangedWay', Postal_Code=8600  WHERE Address_ID=1;
+\echo and with insert permissions the insert into addresses should be accepted
+\prompt 'Press <ENTER> to continue ...' dummy
 INSERT INTO Address(address, district, city_id, phone, postal_code)
    VALUES ('Werk-VI-Strasse 999','Styria',463,'03862/9999','8605');
--- now when switching back to admin user we check the contents of the audit table
+\echo now when switching back to administrative user "Postgres" we check the contents of the audit table
+\prompt 'Press <ENTER> to continue ...' dummy
 set role Postgres;
 SELECT event_id, schema_name, table_name, relid, session_user_name, action_tstamp_tx, action_tstamp_stm, action_tstamp_clk, transaction_id, application_name, client_addr, client_port, client_query, action, row_data, changed_fields, statement_only
 	FROM audit.logged_actions;
+\echo "expected result: the update and insert should both have been logged"
