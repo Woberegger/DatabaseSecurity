d22559fe8bffa4c90ee0f10d7b5355e0b171fb4d
Wolfgang OBEREGGER <Wolfgang.Oberegger@knapp.com>
2026-05-12 22:15:44 +0200

    init.sql does not work with anonymous block

--- scripts/08/docker_cluster/primary/init.sql
+++ scripts/08/docker_cluster/primary/init.sql
@@ -1,26 +1,20 @@
 -- create replication user on primary node and replica database istance
-DO $$
-BEGIN
-   -- avoid exception, if it should be called more than once during startup
-   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'replica') THEN
-      CREATE ROLE replica WITH REPLICATION LOGIN ENCRYPTED PASSWORD 'replica_pass';
-      CREATE DATABASE replica WITH OWNER replica;
-      SET Role replica;
-      CREATE SCHEMA IF NOT EXISTS repschema;
-      SET SCHEMA 'repschema';
-      SELECT current_schema();
-      SET search_path TO repschema, public;
+CREATE ROLE replica WITH REPLICATION LOGIN ENCRYPTED PASSWORD 'replica_pass';
+CREATE DATABASE replica WITH OWNER replica;
+SET Role replica;
+\connect replica;
+CREATE SCHEMA IF NOT EXISTS repschema;
+SET SCHEMA 'repschema';
+SELECT current_schema();
+SET search_path TO repschema, public;
 
-      CREATE TABLE IF NOT EXISTS RepTable (
-         name text,
-         remark text
-      ) tablespace pg_default;
+CREATE TABLE IF NOT EXISTS RepTable (
+   name text,
+   remark text
+) tablespace pg_default;
 
-      INSERT INTO RepTable VALUES ('Rec1', 'first record');
-      INSERT INTO RepTable VALUES ('Rec2', 'second record');
+INSERT INTO RepTable VALUES ('Rec1', 'first record');
+INSERT INTO RepTable VALUES ('Rec2', 'second record');
 
-      GRANT ALL ON SCHEMA repschema TO replica;
-      GRANT ALL ON repschema.RepTable TO replica;
-    END IF;
-END
-$$;
+GRANT ALL ON SCHEMA repschema TO replica;
+GRANT ALL ON repschema.RepTable TO replica;
