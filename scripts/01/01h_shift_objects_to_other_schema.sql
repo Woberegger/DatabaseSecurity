f7b7d207024699d1646ab591a2073687ca60ec70
Wolfgang OBEREGGER <Wolfgang.Oberegger@knapp.com>
2026-05-12 17:27:29 +0200

    sequence owner also needs to be changed

--- scripts/01/01h_shift_objects_to_other_schema.sql
+++ scripts/01/01h_shift_objects_to_other_schema.sql
@@ -3,3 +3,10 @@
 SELECT 'ALTER TABLE '||tablename||' SET Schema dvd;'  FROM pg_catalog.pg_tables where schemaname='public';
 SELECT 'ALTER VIEW '||viewname||' SET Schema dvd;'  FROM pg_catalog.pg_views where schemaname='public';
 SELECT 'ALTER SEQUENCE '||sequencename||' SET Schema dvd;'  FROM pg_catalog.pg_sequences where schemaname='public';
+-- for sequences, which are used for auto-increment, it is important to also modify the relationship of the owning table
+SELECT 'ALTER SEQUENCE ' ||
+   split_part(split_part(column_default, '''', 2), '.', 1) || '.' ||
+   split_part(split_part(column_default, '''', 2), '.', 2) ||
+   ' OWNED BY ' || table_schema || '.' || table_name || '.' || column_name || ';'
+   FROM information_schema.columns
+   WHERE column_default LIKE 'nextval%';
